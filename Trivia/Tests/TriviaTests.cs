2c88d255064ad30e328d3ffc44c5d4dcd932bcff
Philippe Gung <philippe.gung@youscribe.com>
2021-04-12 11:12:32 +0200

    init repo

---
+++ Trivia/Tests/TriviaTests.cs
@@ -0,0 +1,67 @@
+﻿using Assent;
+using System;
+using System.IO;
+using System.Text;
+using Trivia;
+using Xunit;
+
+namespace Tests
+{
+    public class TriviaTests
+    {
+        [Fact]
+        public void RefactoringTests()
+        {
+            var output = new StringBuilder();
+            Console.SetOut(new StringWriter(output));
+
+            Game aGame = new Game();
+            Console.WriteLine(aGame.IsPlayable());
+            aGame.Add("Chet");
+            aGame.Add("Pat");
+            aGame.Add("Sue");
+
+            aGame.Roll(1);
+            aGame.Roll(1);
+            aGame.Roll(1);
+            aGame.Roll(1);
+            aGame.Roll(1);
+            aGame.Roll(1);
+            aGame.Roll(1);
+            aGame.Roll(1);
+            aGame.Roll(1);
+            aGame.Roll(1);
+            aGame.Roll(1);
+            aGame.Roll(1);
+            aGame.Roll(1);
+            aGame.Roll(1);
+
+            aGame.WasCorrectlyAnswered();
+            aGame.WrongAnswer();
+
+            aGame.Roll(2);
+
+            aGame.Roll(6);
+
+            aGame.WrongAnswer();
+
+            aGame.Roll(2);
+
+            aGame.Roll(2);
+
+            aGame.WrongAnswer();
+
+            aGame.WasCorrectlyAnswered();
+            aGame.Roll(1);
+            aGame.WasCorrectlyAnswered();
+
+            var configuration = BuildConfiguration();
+            this.Assent(output.ToString(), configuration);
+        }
+
+        private static Configuration BuildConfiguration()
+        {
+            return new Configuration();
+        }
+    }
+}
